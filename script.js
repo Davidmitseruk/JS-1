22710e952bf7aabb7320c45b5c00e28e8e5b185d
Davidmitseruk <davidmitseruk@gmail.com>
2024-04-26 19:32:32 +0200

    lol

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const x = 5;
-const y = 2;
-const z = 5;
 
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
+const c = "5";
+console.log(Number(c));
+console.log(typeof Number(c));
+
+const b = 'hello';
+console.log(Number(b));
+console.log(typeof Number(b));
+
+console.log(Number.parseInt('20px'));
+console.log(Number.parseInt('puv'))
+console.log(Number.parseFloat('12.57'))
+console.log(Number.parseFloat('20px'))
 
 
 
-const b = 'hello'
-console.log(namber(b))
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2);
+
+console.log((0.1 *10 + 0.2*10) / 10);
+
+console.log(Math.floor(3.8));
+console.log(Math.ceil(1.3));
+console.log(Math.round(1.4));
+console.log(Math.round(99.6));
+console.log(Math.max(15, 100, 25, 48, 152, 14));
+console.log(Math.min(25, 3.5, 69, 26))
+console.log(Math.pow(2, 4))
+console.log(Math.random() * (10 - 1) + 1)

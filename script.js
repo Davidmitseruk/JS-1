ac052c019fdc6806589312ed27ab13408ea0578b
Davidmitseruk <davidmitseruk@gmail.com>
2024-05-15 19:27:09 +0200

    lol

--- script.js
+++ script.js
@@ -1,48 +1,101 @@
 
-const c = "5";
-console.log(Number(c));
-console.log(typeof Number(c));
+// const c = "5";
+//console.log(Number(c));
+// console.log(typeof Number(c));
 
-const b = 'hello';
-console.log(Number(b));
-console.log(typeof Number(b));
+// const b = 'hello';
+// console.log(Number(b));
+// console.log(typeof Number(b));
 
-console.log(Number.parseInt('20px'));
-console.log(Number.parseInt('puv'))
-console.log(Number.parseFloat('12.57'))
-console.log(Number.parseFloat('20px'))
+// console.log(Number.parseInt('20px'));
+// console.log(Number.parseInt('puv'))
+// console.log(Number.parseFloat('12.57'))
+// console.log(Number.parseFloat('20px'))
 
 
 
-console.log(0.1 + 0.2 === 0.3);
-console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2);
 
-console.log((0.1 *10 + 0.2*10) / 10);
+// console.log((0.1 *10 + 0.2*10) / 10);
 
-console.log(Math.floor(3.8));
-console.log(Math.ceil(1.3));
-console.log(Math.round(1.4));
-console.log(Math.round(99.6));
-console.log(Math.max(15, 100, 25, 48, 152, 14));
-console.log(Math.min(25, 3.5, 69, 26));
-console.log(Math.pow(2, 4));
-console.log(Math.random() * (10 - 1) + 1);
+// console.log(Math.floor(3.8));
+// console.log(Math.ceil(1.3));
+// console.log(Math.round(1.4));
+// console.log(Math.round(99.6));
+// console.log(Math.max(15, 100, 25, 48, 152, 14));
+// console.log(Math.min(25, 3.5, 69, 26));
+// console.log(Math.pow(2, 4));
+// console.log(Math.random() * (10 - 1) + 1);
 
-const a = ('welcome to bahamas!');
-console.log(a.startsWith('w'));
-console.log(a.endsWith('s'));
+// const a = ('welcome to bahamas!');
+// console.log(a.startsWith('w'));
+// console.log(a.endsWith('s'));
 
-console.log(a.padEnd(22, "!" ));
-console.log(a.padStart(20, "!" ));
+// console.log(a.padEnd(22, "!" ));
+// console.log(a.padStart(20, "!" ));
 
-const s = '   hello    ';
-console.log(s.trim());
+// const s = '   hello    ';
+// console.log(s.trim());
 
-const name = 'David';
-const age = 13;
+// const name = 'David';
+// const age = 13;
 
-const message = `my name is ${name},and i am ${age} years old`
-console.log(message)
+// const message = `my name is ${name},and i am ${age} years old`
+// console.log(message)
 // const message = 'My name is' + ' ' + name + ' ' +'i am' + ' ' + age + 'years old.';
 // console.log(message);
 
+// let value = 'hello';
+// let boolValue = Boolean(value);
+// console.log(boolValue);
+
+
+// const number = 15;
+// const result = number > 10 && number < 20;
+// console.log(result);
+
+// const number = 15;
+// const result = number > 16 && number < 20;
+
+// const number = 15;
+// const result = number < 20 || number > 30;
+// console.log(result);
+
+// console.log(!true);
+// console.log(!0);
+
+
+// let cost = 0;
+// const sub = 'normal';
+// if(sub === 'pro') {
+//     cost = 100
+// }
+// console.log(cost)
+
+// let cost;
+// const sub = 'pro';
+// if (sub === 'pro'){
+ //    cost = 100;
+    
+// }
+// else {
+ //    cost = 0;
+// }
+
+let cost;
+const sub = 'premium';
+
+if (sub === 'normal'){
+    cost = 0
+}
+else if(sub === 'pro'){
+    cost = 100
+}
+else if (sub === 'premium'){
+    cost = 200
+}
+else {
+    console.log('fail')
+}
+console.log(cost)

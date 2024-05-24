d9820ba8dc5755cfa539c9afa5fd6d073f51284d
Davidmitseruk <davidmitseruk@gmail.com>
2024-05-24 19:27:11 +0200

    lol

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-let number = 0;
-while (number < 10){
-    console.log('number:', number);
-    number += 1;
-}
+// let number = 0;
+// while (number < 10){
+//    console.log('number:', number);
+//    number += 1;
+// }
 
+// let password = '';
+
+// do {
+//     password = prompt('Password requires 5 sympols', '');
+
+// } while (password.length < 6);
+// console.log('password:', password);
+
+
+// const max = 5;
+
+// for (let i = 0; i < max; i += 1){
+//     console.log(i);
+// }
+
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i +=1) {
+//     sum =+ i;
+// }
+// console.log(sum);
+
+// for(let i = 0; 1 < 10; 1 += 1){
+ //    if (i === 6){
+ //        brake
+ //    }
+// }
+
+const sum = 10;
+for (let i = 0; 1 < sum; 1 +=1){
+    if( i % 2 === 0){
+        continue;
+    }
+    console.log('Something:', i);
+}

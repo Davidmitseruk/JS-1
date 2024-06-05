9a3f28ddd066b7f852b25716a49f42ab1b7a2569
Davidmitseruk <davidmitseruk@gmail.com>
2024-06-05 20:55:53 +0200

    lololol

--- index.js
+++ index.js
@@ -12,3 +12,44 @@ console.log(arr.length);
 
 const lastIndex= fruits.length - 1;
 console.log(lastIndex);
+
+
+
+const string = 'hello, my friends!';
+console.log(string.split(' '));
+
+const name = ['Alex', 'David', 'Nika', 'Bob', 'Yahna', 'Arthur'];
+console.log(name.join(','))
+
+const names = ['Alex', 'David', 'Nika', 'Bob', 'Yahna', 'Arthur'];
+console.log(names.indexOf('Bebee'));
+
+const navne = ['Alex', 'David', 'Nika', 'Bob', 'Yahna', 'Arthur'];
+console.log(navne.includes('David'));
+
+const fruit = ['apple', 'kiwi', 'mango'];
+const frugt = 'mango';
+
+if (fruit.includes(frugt)){
+
+    console.log('yes')
+
+} else (console.log('no'));
+
+const nummer = [1, 2];
+nummer.push(5);
+console.log(nummer);
+
+nummer.push(8);
+console.log(nummer)
+
+nummer.push('one');
+console.log(nummer)
+
+console.log(nummer.pop());
+
+const navn = ['Alex', 'David', 'Nika', 'Bob', 'Yahna', 'Arthur'];
+console.log(navn.shift())
+
+navn.unshift('Bogdan');
+console.log(navn);

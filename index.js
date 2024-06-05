debca55923e50165749207feed9a36399fdd3e43
Davidmitseruk <davidmitseruk@gmail.com>
2024-06-05 19:16:55 +0200

    lolololololololo

---
+++ index.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+const fruits = ['apple', 'kiwio', 'mango'];
+console.log(fruits);
+
+const arr =  ['cat', 'dog', 'bird']; 
+arr[10]= 'fish';
+console.log(arr);
+
+console.log(arr.length);
+
+const lastIndex= fruits.length - 1;
+console.log(lastIndex);

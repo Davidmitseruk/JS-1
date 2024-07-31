94730411d8fcf9e726c241ef5d8484642b1210b1
Davidmitseruk <davidmitseruk@gmail.com>
2024-07-31 10:43:04 +0200

    llol

--- script.js
+++ script.js
@@ -347,17 +347,17 @@
  // });
  // console.log(nummers);
 
- const numbers = [17, 52, 65, 41, 12, 9];
- const sum = numbers.reduce((acc, value)=> acc + value, 0);
- console.log(sum); 
+  //const numbers = [17, 52, 65, 41, 12, 9];
+  //const sum = numbers.reduce((acc, value)=> acc + value, 0);
+  //console.log(sum); 
 
- const tweets = [
-    {id: '012', likes: 23, tags: ['css', 'sass']},
-    {id: '017', likes: 33, tags: ['js', 'javascript']},
-    {id: '018', likes: 87, tags: ['css', 'html']},
-    {id: '011', likes: 47, tags: ['react', 'js']},
-    {id: '019', likes: 98, tags: ['nodejs', 'js']},
- ]
+  //const tweets = [
+  //   {id: '012', likes: 23, tags: ['css', 'sass']},
+  //   {id: '017', likes: 33, tags: ['js', 'javascript']},
+   //  {id: '018', likes: 87, tags: ['css', 'html']},
+   //  {id: '011', likes: 47, tags: ['react', 'js']},
+   //  {id: '019', likes: 98, tags: ['nodejs', 'js']},
+  //]
   //const likes = tweets.reduce((totallikes, tweet)  => totallikes + tweet.likes, 0)
   //console.log(likes)
 
@@ -371,27 +371,94 @@
   //}, []);
   //console.log(tags)
 
-  const tagsSum = tweets =>tweets.reduce((alltags, tweet) => {
-    alltags.push(...tweet.tags)
-      return alltags;
-    }, []);
-    const tags = tagsSum(tweets);
-    const tgasCount = (acc, tag) => {
-        if(!acc.hasOwnProperty(tag)){
-            acc[tag] = 0;
+   //const tagsSum = tweets =>tweets.reduce((alltags, tweet) => {
+   //  alltags.push(...tweet.tags)
+   //    return alltags;
+   //  }, []);
+   //  const tags = tagsSum(tweets);
+   //  const tgasCount = (acc, tag) => {
+   //      if(!acc.hasOwnProperty(tag)){
+   //          acc[tag] = 0;
+ //
+   //      }
+   //      acc[tag] += 1;
+   //      return acc;
+   //  };
+   //  const totalTags = tags => tags.reduce(tgasCount, {});
+   //  const tagcount = totalTags(tags);
+   // //  console.log(tagcount);
 
-        }
-        acc[tag] += 1;
-        return acc;
-    };
-    const totalTags = tags => tags.reduce(tgasCount, {});
-    const tagcount = totalTags(tags);
-    console.log(tagcount);
+
+   //  const nummer = [5, 6, 1, 3, 4, 2];
+   //  console.log(nummer.sort());
+ //
+   // //  const names = ['Alex', 'Nick', 'Bob', 'Jhon'];
+   //  console.log(names.sort());
 
 
-    const nummer = [5, 6, 1, 3, 4, 2];
-    console.log(nummer.sort());
 
-    const names = ['Alex', 'Nick', 'Bob', 'Jhon'];
-    console.log(names.sort());
-    
+      //   const letters = [5, 6, 1, 3, 4, 2];
+    //  console.log(letters.sort()) // ['A', 'F', 'a', 'h']
+     //    const newLetters = letters.sort((curEl, nextEl) =>{
+    //       return curEl - nextEl
+    //     //     });
+    //     console.log(newLetters);
+
+
+  //  const fruits = ['kiwi', 'apple', 'orange', 'banana'];
+    //     fruits.sort(function(a, b){
+     //      return a.localeCompare(b)
+     //    })
+     //    console.log(fruits);
+
+    //    const copy = [...fruits];
+    //    console.log(copy);
+    //    console.log(copy === fruits);
+
+
+          //const num = [5, 6, 1, 3, 4, 2];
+      //const even = num.filter(x => x%2 === 0);
+      //console.log(even);
+      //const nezNum = even.map (x => x * 3);
+      //console.log(nezNum);
+      //const reverse = nezNum.reverse();
+      //console.log(reverse);
+            //const result = num
+            //.filter(x => x%2 === 0)
+            //.map(z => z * 3)
+            //.reverse();
+            //console.log(result);
+
+
+
+
+      //const players = [
+        //{ id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+        //{ id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+        //{ id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+        //{ id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+        //{ id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+        //];
+
+        //players.sort((a, b) => a.timePlayed - b.timePlayed)
+        //console.log(players)
+
+
+
+const arr = [
+{ name: 'John', age: 32 },
+{ name: 'Jane', age: 26 },
+{ name: 'Mike', age: 42 },
+{ name: 'Emily', age: 29 }
+];
+
+arr.sort((a, b) => {
+  if(a.name < b.name){
+    return -1;
+  }
+  if( a.name > b.name){
+    return 1;
+  }
+  return 0;
+})
+console.log(arr);
